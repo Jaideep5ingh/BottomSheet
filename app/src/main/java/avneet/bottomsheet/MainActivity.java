68089db4c6f885b3a2bf88d19c9e3cda39c92ae1
Avneet Kaur <avkaur79@gmail.com>
2017-04-22 18:44:35 +0530

    hdge

--- app/src/main/java/avneet/bottomsheet/MainActivity.java
+++ app/src/main/java/avneet/bottomsheet/MainActivity.java
@@ -1,13 +1,27 @@
 package avneet.bottomsheet;
 
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+   FloatingActionButton fab3,fab4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fab3=(FloatingActionButton) findViewById(R.id.floatingActionButton2);
+        fab4=(FloatingActionButton)findViewById(R.id.floatingActionButton4);
+        View parentView = getLayoutInflater().inflate(R.layout.dialog,null);
+        BottomSheetDialog.setContentView(parentView);
+        BottomSheetBehavior bottomSheetBehavior=BottomSheetBehavior.from((View)parentView.getParent());
+
+
     }
 }
