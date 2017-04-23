eb96abe56d4f18da86666c176aec1ceaa00a413f
Avneet Kaur <avkaur79@gmail.com>
2017-04-23 13:46:57 +0530

    hgntn

--- app/src/main/java/avneet/bottomsheet/MainActivity.java
+++ app/src/main/java/avneet/bottomsheet/MainActivity.java
@@ -10,21 +10,45 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-   FloatingActionButton fab3,fab4;
-    BottomSheetDialog bottomSheetDialog;
+    FloatingActionButton fab3, fab4;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fab3=(FloatingActionButton) findViewById(R.id.floatingActionButton2);
-        fab4=(FloatingActionButton)findViewById(R.id.floatingActionButton4);
-        View parentView = getLayoutInflater().inflate(R.layout.dialog,null);
-        bottomSheetDialog.setContentView(parentView);
-        BottomSheetBehavior bottomSheetBehavior=BottomSheetBehavior.from((View)parentView.getParent());
-        bottomSheetBehavior.setPeekHeight((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,100,getResources().getDisplayMetrics()));
-        bottomSheetDialog.show();
+        fab3 = (FloatingActionButton) findViewById(R.id.floatingActionButton2);
+        fab4 = (FloatingActionButton) findViewById(R.id.floatingActionButton4);
 
+        fab3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
+                View parentView = getLayoutInflater().inflate(R.layout.dialog, null);
+                bottomSheetDialog.setContentView(parentView);
+                BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from((View) parentView.getParent());
+                //bottomSheetBehavior.setPeekHeight((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, getResources().getDisplayMetrics()));
+                bottomSheetBehavior.setPeekHeight(BottomSheetBehavior.PEEK_HEIGHT_AUTO);
+
+                bottomSheetDialog.show();
+
+
+            }
+
+
+        });
+
+        fab4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
+                View parentView = getLayoutInflater().inflate(R.layout.dialog, null);
+                bottomSheetDialog.setContentView(parentView);
+                BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from((View) parentView.getParent());
+                //bottomSheetBehavior.setPeekHeight((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, getResources().getDisplayMetrics()));
+                bottomSheetBehavior.setPeekHeight(BottomSheetBehavior.PEEK_HEIGHT_AUTO);
+                bottomSheetDialog.show();
+            }
+        });
     }
 }
